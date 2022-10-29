8c16c3750f958b2b46bb4019bd3f7e0ba0288db9
Joao Euko <joaopeuko@gmail.com>
2022-10-29 14:41:29 -0300

    Update docstring

--- gsi/app/predict/controller.py
+++ gsi/app/predict/controller.py
@@ -19,10 +19,10 @@ def predictor(input_data: InputPredict) -> dict:
     This function call the prediction
     Args:
         input_data:
-            It is the values that needs to ebe predicted
+            It is the values that needs to be predicted
 
     Returns:
-        It returns the prediction
+        It returns the prediction and some input parameters
 
     """
     log.logger.info(f'calling the predictor with variables {input_data}')
@@ -43,6 +43,16 @@ def predictor(input_data: InputPredict) -> dict:
 
 
 def get_issues_numbers(input_data: InputPredict) -> list:
+    """
+    This function get the last amount of issues page set by the user.
+    Args:
+        input_data:
+            It is the model input, that contain number of pages, repository and repository owner
+
+    Returns:
+        It returns a list contain all the issues number
+
+    """
     issues_numbers = []
     for page in range(input_data.pages_amount):
         url = parameters['GITHUB_BASE_API_URL'] + f'repos/{input_data.owner}/{input_data.repo}/issues?page={page}'
@@ -52,7 +62,20 @@ def get_issues_numbers(input_data: InputPredict) -> list:
     return issues_numbers
 
 
-def get_comments(input_data: InputPredict, issues_numbers: list) -> list:
+def get_comments(input_data: InputPredict, issues_numbers: list) -> list[dict]:
+    """
+    It consumes the function get_issues_numbers to retrieve the issues comments.
+    Args:
+        input_data:
+            It is the model input, that contain number of pages, repository and repository owner
+        issues_numbers:
+            It is a list, the return of the function get_issues_numbers, that contains the issues list
+
+    Returns:
+        It returns a list of dictionaries that contains the issue number and comment
+
+
+    """
     issues_comments = []
     for number in issues_numbers:
         url = parameters['GITHUB_BASE_API_URL'] + f'repos/{input_data.owner}/{input_data.repo}/issues/{number}/comments'
